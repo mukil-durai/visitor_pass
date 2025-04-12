49b0f5c5a2224b120b175e7e3903a167dea8ecce
mukildurai <mukild.22it@kongu.edu>
2025-04-12 22:25:10 +0530

    fifth  commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/visitor_pass/', // Add this line for GitHub Pages
+  base: '/visitor_pass/', // Matches your GitHub Pages deployment path
   plugins: [react()],
 });
