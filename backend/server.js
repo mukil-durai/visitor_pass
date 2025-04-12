b14ff84dc9b401d236c082ef02ca8bd05816d978
mukildurai <mukild.22it@kongu.edu>
2025-04-12 23:38:49 +0530

    last commit

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'https://your-frontend-url.com', // Replace with your frontend's URL
+  origin: 'https://mukil-durai.github.io', // Replace with your frontend's URL
 }));
 
 mongoose.connect(process.env.MONGO_URI);
